8a7a81b18ac1c7ad128038260a34b0483ad3011c
Vitaliy-1915 <74369854+Vitaliy-1915@users.noreply.github.com>
2021-05-25 03:11:09 +0300

    Update axiosCountries.js

--- src/js/axiosCountries.js
+++ src/js/axiosCountries.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://restcountries.eu';
 
 const fetchCountries = (name) => {
-    let params = '/rest/v2/name/?{name}';
+    let params = '/rest/v2/name/usa';
     return axios.get(params)
         .then(response => response = response.data)
    
